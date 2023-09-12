3e0b6f0a51e2fdf4101ec516bb3cf5b54fecb492
Csaba Soós <csabi.soos.05@gmail.com>
2023-09-12 12:21:13 +0200

    ora vege

--- allatkert/Program.cs
+++ allatkert/Program.cs
@@ -10,7 +10,29 @@ namespace allatkert
     {
         static void Main(string[] args)
         {
-
+            #region beolvasas
+            string sor = Console.ReadLine();
+            string[] sortomb = sor.Split(' ');
+            int N = int.Parse(sortomb[0]);
+            int M = int.Parse(sortomb[1]);
+            List<List<int>> lista = new List<List<int>>();
+            for (int i = 1; i < M; i++){
+                sor = Console.ReadLine();
+                sortomb = sor.Split(' ');
+                List<int> par = new List<int> { int.Parse(sortomb[0]), int.Parse(sortomb[1])};
+                lista.Add(par);
+            }
+            #endregion
         }
     }
 }
+/**peldabemenet /
+5 7 
+0 1
+1 4
+3 1
+3 5
+2 0
+2 3
+1 2
+/**/

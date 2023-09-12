7d912c0a154a141344eb7c8cfe49bda2f7896807
Csaba Soós <csabi.soos.05@gmail.com>
2023-09-12 13:06:59 +0200

    ora vege, meg hibas

--- allatkert/Program.cs
+++ allatkert/Program.cs
@@ -15,14 +15,78 @@ namespace allatkert
             string[] sortomb = sor.Split(' ');
             int N = int.Parse(sortomb[0]);
             int M = int.Parse(sortomb[1]);
-            List<List<int>> lista = new List<List<int>>();
-            for (int i = 1; i < M; i++){
+            List<List<int>> ellista = new List<List<int>>();
+            for (int i = 0; i < M; i++)
+            {
                 sor = Console.ReadLine();
                 sortomb = sor.Split(' ');
-                List<int> par = new List<int> { int.Parse(sortomb[0]), int.Parse(sortomb[1])};
-                lista.Add(par);
+                List<int> par = new List<int> { int.Parse(sortomb[0]), int.Parse(sortomb[1]) };
+                ellista.Add(par);
             }
+            //ellenorzes:
+            /*
+            for (int i = 0; i < ellista.Count; i++)
+            {
+                Console.Write(ellista[i][0]);
+                Console.Write(" ");
+                Console.WriteLine(ellista[i][1]);
+            }
+            /**/
+            Console.WriteLine();
             #endregion
+
+            Dictionary<int, int> szotar = new Dictionary<int, int>();
+            foreach (List<int> par in ellista)
+            {
+                for (int i = 0;i < 2;i++)
+                {
+                    if (szotar.ContainsKey(par[i]))
+                    {
+                        szotar[par[i]] += 1;
+                    }
+                    else
+                    {
+                        szotar[par[i]] = 1;
+                    }
+                }
+            }
+            //ellenorzes
+            /*
+            Console.WriteLine("csucs/fokszam szotar:");
+            foreach (int kulcs in szotar.Keys)
+            {
+                Console.Write(kulcs);
+                Console.Write(" ");
+                Console.WriteLine(szotar[kulcs]);
+            }
+            /**/
+
+            //hany olyan sor van, ahol a masodik elem egyes?
+            
+            int db = 0;
+            foreach (int kulcs in szotar.Keys)
+            {
+                if (szotar[kulcs] == 1)
+                {
+                    db++;
+                }
+            }
+            Console.Write(db);
+            /**/
+            //melyik sorszamnak van a legnagyobb fokszama?
+            //keressuk azt a kulcsot, amelyiknek a legnagyobb az erteke
+
+            int maxkulcs = szotar.Keys.First();
+            foreach (int kulcs in szotar.Keys) 
+            {
+                if (szotar[maxkulcs] < szotar[kulcs])
+                {
+                    maxkulcs = kulcs;
+
+                }
+            }
+            Console.Write(" ");
+            Console.WriteLine(maxkulcs);
         }
     }
 }
